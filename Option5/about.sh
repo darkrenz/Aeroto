4ca03fc970d25e5de833d827a4b2a5879760db89
NoteX <darkrenz01@gmail.com>
2023-12-25 16:37:08 +0800

    Add files via upload

---
+++ Option5/about.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+
+echo -e "${tyblue}┌──────────────────────────────────────────┐${NC}"
+echo -e "${tyblue}         Aeroto Script Modified by Tojirenz                 ${NC}"
+echo -e "${tyblue}└──────────────────────────────────────────┘${NC}"
+echo ""
+echo -e "${tyblue}┌──────────────────────────────────────────┐${NC}"
+echo -e "${tyblue}           This script is only suited for                       ${NC}"
+echo -e "${tyblue}           Debian 9/10 64bit                        ${NC}"
+echo -e "${tyblue}           Ubuntu 18.04/20.04 64bit                        ${NC}"
+echo -e "${tyblue}           Mikrotik Friendly.                      ${NC}"
+echo -e "${tyblue}└──────────────────────────────────────────┘${NC}"
+echo ""
+echo -e "${tyblue}┌──────────────────────────────────────────┐${NC}"
+echo -e "${tyblue}         Hindi ko ginawa itong script para ibenta                 ${NC}"
+echo -e "${tyblue}         Malaya ka na ipadecrypt itong script                  ${NC}"
+echo -e "${tyblue}         Pero pahihirapan muna kita kagaya nang                  ${NC}"
+echo -e "${tyblue}         Paano ko minano mano yung pag eedit nito                  ${NC}"
+echo -e "${tyblue}└──────────────────────────────────────────┘${NC}"
